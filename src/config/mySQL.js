53e1055288992987f77bbbefe5f2488b1135ebcb
dhiyo7 <dhiyoreksa12@gmail.com>
2021-02-01 06:28:56 +0700

    Fix Connection DB

--- src/config/mySQL.js
+++ src/config/mySQL.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 // koneksi ke db
 
-const {MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE} = process.env
+const {MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DATABASE} = process.env
 const db = mysql.createConnection({
-    MYSQL: MYSQL_HOST,
+    host: MYSQL_HOST,
     user: MYSQL_USER,
-    password: MYSQL_PASSWORD,
+    password: MYSQL_PASS,
     database: MYSQL_DATABASE
 })
 
