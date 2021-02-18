65d35a1d14846c16f4a8e15f45e167b1c999e0b4
bronya96 <bronyazaychik96@gmail.com>
2021-02-18 21:04:46 +0700

    fix history + add sql

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -12,25 +12,25 @@ module.exports = {
     },
     getBalanceInOut: (req, res) => {
         const { id } = req.decodedToken
-        const {from, to, today, thisWeek, thisMonth} = req.query
-        let {flow} = req.query
-        let additionalQuery=''
-        if(from !=null && to !=null){
+        const { from, to, today, thisWeek, thisMonth } = req.query
+        let { flow } = req.query
+        let additionalQuery = ''
+        if (from != null && to != null) {
             additionalQuery = `AND t.created_at BETWEEN '${from} 00:00:00' AND '${to} 23:59:59'`
         }
-        if(today){
-            additionalQuery = `AND t.created_at >= CURRENT_DATE` 
+        if (today) {
+            additionalQuery = `AND t.created_at >= CURRENT_DATE`
         }
-        if(thisWeek){
-            additionalQuery = `AND WEEK(t.created_at,1)  = WEEK(CURRENT_DATE,1) AND YEAR(t.created_at) = YEAR(CURRENT_DATE)`
+        if (thisWeek) {
+            additionalQuery = `AND WEEK(t.created_at,1)  = WEEK(CURRENT_DATE,1) AND YEAR(t.created_at) = YEAR(CURRENT_DATE) AND NOT t.created_at >= CURRENT_DATE`
         }
-        if(thisMonth){
-            additionalQuery = `AND MONTH(t.created_at) = MONTH(CURRENT_DATE) AND YEAR(t.created_at) = YEAR(CURRENT_DATE)`
+        if (thisMonth) {
+            additionalQuery = `AND MONTH(t.created_at) = MONTH(CURRENT_DATE) AND YEAR(t.created_at) = YEAR(CURRENT_DATE) AND NOT WEEK(t.created_at,1)  = WEEK(CURRENT_DATE,1)`
         }
-        if(flow == null){
+        if (flow == null) {
             flow = 'out'
         }
-        homeModel.getBalanceInOut(id, additionalQuery,flow)
+        homeModel.getBalanceInOut(id, additionalQuery, flow)
             .then((result) => {
                 res.status(result.status).json(result)
             }).catch((error) => {
@@ -39,33 +39,33 @@ module.exports = {
     },
     getAllTranfer: (req, res) => {
         const { id } = req.decodedToken
-        const {from, to, today, thisWeek, thisMonth} = req.query
-        let additionalQuery=''
-        if(from !=null && to !=null){
+        const { from, to, today, thisWeek, thisMonth } = req.query
+        let additionalQuery = ''
+        if (from != null && to != null) {
             additionalQuery = `AND t.created_at BETWEEN '${from} 00:00:00' AND '${to} 23:59:59'`
         }
-        if(today){
+        if (today) {
             additionalQuery = `AND t.created_at >= CURRENT_DATE`
         }
-        if(thisWeek){
-            additionalQuery = `AND WEEK(t.created_at,1) = WEEK(CURRENT_DATE,1) AND YEAR(t.created_at) = YEAR(CURRENT_DATE)`
+        if (thisWeek) {
+            additionalQuery = `AND WEEK(t.created_at,1) = WEEK(CURRENT_DATE,1) AND YEAR(t.created_at) = YEAR(CURRENT_DATE) AND NOT t.created_at >= CURRENT_DATE`
         }
-        if(thisMonth){
-            additionalQuery = `AND MONTH(t.created_at) = MONTH(CURRENT_DATE) AND YEAR(t.created_at) = YEAR(CURRENT_DATE)`
+        if (thisMonth) {
+            additionalQuery = `AND MONTH(t.created_at) = MONTH(CURRENT_DATE) AND YEAR(t.created_at) = YEAR(CURRENT_DATE) AND NOT WEEK(t.created_at,1)  = WEEK(CURRENT_DATE,1)`
         }
         Promise.all([
-            homeModel.getBalanceInOut(id, additionalQuery,'in'), // 7 row
-            homeModel.getBalanceInOut(id, additionalQuery,'out') // 7 row
+            homeModel.getBalanceInOut(id, additionalQuery, 'in'), // 7 row
+            homeModel.getBalanceInOut(id, additionalQuery, 'out') // 7 row
         ]).then((result) => {
             let newTranfer = result[0].data.concat(result[1].data)
-            
+
             newTranfer = newTranfer.sort((a, b) => { //sort date desc
                 return b.id - a.id
             })
-            newTranfer = newTranfer.filter((value, index) =>{
+            newTranfer = newTranfer.filter((value, index) => {
                 return index < 7  // biar cuma 7
             })
-    
+
             res.status(200).json({
                 status: 200,
                 message: `Berhasil menampilkan data`,
